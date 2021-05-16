4ae6e95efe79ae3c0f8058d4cfcf11a3e48085ab
Artem <arpavlovscky@yandex.ru>
2021-05-16 11:54:49 +0300

    push

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(function() {
+    
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        windowW = $(window).innerWidth(),
+
+        scrollOffset = $(window).scrollTop();
+
+    $(window).on("scroll", function() {
+        scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+    })
+
+
+    //  smooth scroll
+    $("[data-scroll]").on('click', function(event) {
+        event.preventDefault();
+
+        var blockId = $(this).data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $('html, body').animate ({
+            scrollTop: blockOffset - 250
+        })
+
+    })
+
+
+    function checkScroll(scrollOffset) {
+        if (scrollOffset >= introH) {
+            header.addClass('fixed')
+        } else {
+            header.removeClass('fixed')
+        }
+    }
+
+    
+    // menu hamburger
+    $("#nav-toggle").on('click', function(event) {
+        event.preventDefault();
+
+        $('#nav').toggleClass('active')
+        
+    });
+});
